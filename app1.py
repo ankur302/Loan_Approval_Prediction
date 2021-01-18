2653f7414eb45f1b43a79bfd011a9d0e4a9d8994
ankur302 <65133679+ankur302@users.noreply.github.com>
2021-01-18 22:19:49 +0530

    Add files via upload

--- app1.py
+++ app1.py
@@ -8,10 +8,9 @@ Created on Fri Jan 15 11:45:17 2021
 
 import streamlit as st
 import numpy as np
-from PIL import Image
 import pickle
 
-pickle_in = open('loan_prediction.pkl','rb')
+pickle_in = open('loan_prediction1.pkl','rb')
 rfc = pickle.load(pickle_in)
 
 def welcome():
@@ -49,34 +48,38 @@ def model_predict2(Encoded_Property_Area,ApplicantIncome,CoapplicantIncome,LoanA
   x[4] = Loan_Amount_Term
   x[5] = Credit_History
 
-  pred =  rfc.predict([x])[0]
   
-  if pred == 1:
-      return "Approved"
-  else:
-      return 'Not Approved'
+  mapping = {0:'Not Approved',
+             1:'Approved'}
+  return mapping[rfc.predict([x])[0]]
+  
+  
 
 def main():
     st.title("Loan Approval Prediction")
     html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit Loan Approval Prediction App</h2>
+    <div style="background-color:Blue;padding:20px">
+    <h2 style="color:white;text-align:center;">Loan Approval Prediction App</h2>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
     Property_Area_location = st.selectbox("Property Area Location",['Rural','Urban','SemiUrban'])
     ApplicantIncome = st.slider("Applicant's Yearly Income(in hundreds)",min_value = 0,max_value = 81000,value = 1000,step = 81)
+    st.write("Your value is ",ApplicantIncome)
     CoapplicantIncome = st.slider("Yearly Income of Coapplicant(in hundreds)",min_value = 0,max_value = 42000,value = 1000,step = 42)
+    st.write("Your value is ",CoapplicantIncome)
     LoanAmount = st.slider("Loan Amount(in 1k)",min_value = 0,max_value = 800,value = 50,step = 16)
+    st.write("Your value is ",LoanAmount)
     Loan_Amount_Term = st.slider("Term of the Loan Amount(in days)",min_value = 0,max_value = 360,value =90,step = 4)
+    st.write("Your value is ",Loan_Amount_Term)
     Credit_History = st.selectbox("Credit History",['Has All debts Paid','Not Paid'])
     result=""
     if st.button("Predict"):
         result=model_predict2(Property_Area_location,ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History)
     st.success('Your Loan will be {}'.format(result))
     if st.button("About"):
-        st.text("Lets LEarn")
-        st.text("Built with Streamlit")
+        st.text("Made With Streamlit")
+        
 
 if __name__=='__main__':
     main()
